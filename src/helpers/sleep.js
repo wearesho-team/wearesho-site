5815d7e4fe55c119debb35b3dbd9a2d2b7a053c9
Dmitry <makarenko.dmitry@wearesho.com>
2017-08-14 17:49:51 +0300

    Tests done

--- src/helpers/sleep.js
+++
@@ -1,4 +0,0 @@
-export default (ms = 1000) => {
-    return new Promise(r => setTimeout(r, ms));
-};
-//# sourceMappingURL=sleep.js.map
--- tests/unit/layout-specs.tsx
+++ tests/unit/layout-specs.tsx
@@ -7,6 +7,9 @@ import {createMemoryHistory, History} from 'history';
 import {Layout} from '../../src/components/Layout';
 import PreLoader from '../../src/components/PreLoader';
 import {MainPage} from "../../src/components/pages/MainPage/MainPage";
+import {ContactPage} from "../../src/components/pages/ContactPage/ContactPage";
+import {SideBar} from "../../src/components/SideBar";
+import {Header} from "../../src/components/Header";
 const preLoaderElement = document.getElementById('pre-loader');
 
 describe('<Layout>', () => {
@@ -22,8 +25,24 @@ describe('<Layout>', () => {
         );
     });
 
-    it('should render MainPage on `/`', () => {
-        expect(wrapper.contains(<MainPage/>)).to.be.true;
+    it('should render <MainPage/> on `/`', () => {
+
+        expect(wrapper).to.containMatchingElement(<MainPage/>);
     });
+
+    it('should render <ContactPage/> on `/contact`', () => {
+
+        history.push('/contact');
+        expect(wrapper).to.containMatchingElement(<ContactPage/>);
+    });
+
+    it('should contain <SideBar/> and <Header/> on each page', () => {
+
+        expect(wrapper).to.containMatchingElement(<SideBar/>);
+        expect(wrapper).to.containMatchingElement(<Header/>);
+        history.push('/contact');
+        expect(wrapper).to.containMatchingElement(<SideBar/>);
+        expect(wrapper).to.containMatchingElement(<Header/>);
+    })
 
 });
