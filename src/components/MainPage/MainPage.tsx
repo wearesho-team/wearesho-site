d3b380dd3a2202c92f88eb671b798adb652cef0d
eugene_wearesho <grishchenko.eugene@wearesho.com>
2017-08-18 18:24:32 +0300

    Projects chronology layout almost finished

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -25,31 +25,103 @@ export class MainPage extends React.Component<undefined, undefined> {
                         </span>
                     </span>
                 </h2>
-                <div className="section-half">
-                    <h4 className="section__subtitle section__subtitle_marker">Реализуем высокотехнологичные проекты</h4>
-                    <ul className="services-list">
-                        <li className="services-list__item">разработка сайтов</li>
-                        <li className="services-list__item">брендинг и дизайн</li>
-                        <li className="services-list__item">формирование ERP и CRM-систем</li>
-                        <li className="services-list__item">аналитика и реклама</li>
-                        <li className="services-list__item">техническая поддержка проектов</li>
-                    </ul>
+                <div className="container">
+                    <div className="section-half">
+                        <h4 className="section__subtitle section__subtitle_marker">Реализуем высокотехнологичные
+                            проекты</h4>
+                        <ul className="services-list">
+                            <li className="services-list__item">разработка сайтов</li>
+                            <li className="services-list__item">брендинг и дизайн</li>
+                            <li className="services-list__item">формирование ERP и CRM-систем</li>
+                            <li className="services-list__item">аналитика и реклама</li>
+                            <li className="services-list__item">техническая поддержка проектов</li>
+                        </ul>
+                    </div>
+                    <div className="section-half">
+                        <h4 className="section__subtitle section__subtitle_marker">
+                            Более 3 лет
+                            <span className="section__subtitle_reduced">профессионального сотрудничества с финансовыми компаниями:</span>
+                        </h4>
+                        <ul className="clients-list">
+                            <li className="clients-list__item">
+                                <a href="#" className="clients-list__link">NIKO Holding</a>
+                                <span className="clients-list__text">#автодилер&nbsp;#логистика&nbsp;#финансы</span>
+                            </li>
+                            <li className="clients-list__item">
+                                <a href="#" className="clients-list__link">Infinance</a>
+                                <span className="clients-list__text">#кредитование&nbsp;#финансы</span>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-                <div className="section-half">
-                    <h4 className="section__subtitle section__subtitle_marker">
-                        Более 3 лет
-                        <span className="section__subtitle_reduced">профессионального сотрудничества с финансовыми компаниями:</span>
-                    </h4>
-                    <ul className="clients-list">
-                        <li className="clients-list__item">
-                            <a href="#" className="clients-list__link">NIKO Holding</a>
-                            <span className="clients-list__text">#автодилер&nbsp;#логистика&nbsp;#финансы</span>
-                        </li>
-                        <li className="clients-list__item">
-                            <a href="#" className="clients-list__link">Infinance</a>
-                            <span className="clients-list__text">#кредитование&nbsp;#финансы</span>
-                        </li>
-                    </ul>
+                <div className="projects-chronology">
+                    <div className="projects-chronology__division-outer projects-chronology__division-outer_left"></div>
+                    <div className="projects-chronology__division-outer projects-chronology__division-outer_right"></div>
+                    <div className="chronology-slider">
+                        <div className="chronology-slider__body"></div>
+                        <div className="chronology-slider__description">
+                            <div className="chronology-slider__projects">
+                                <a href="#">NIKO</a>&nbsp;/&nbsp;
+                                <a href="#">Infinance</a>
+                            </div>
+                            <span className="chronology-slider__services">Техническая поддержка</span>
+                            <span className="chronology-slider__date">18.08.2017</span>
+                        </div>
+                    </div>
+                    <div className="projects-chronology__item">
+                        <span className="projects-chronology__division projects-chronology__division_left-indent"/>
+                        <span className="projects-chronology__division projects-chronology__division_left-indent"/>
+                        <span className="projects-chronology__division projects-chronology__division_left-indent"/>
+                        <span className="projects-chronology__division projects-chronology__division_wide">2014</span>
+                        <span className="projects-chronology__division projects-chronology__division_right-indent"/>
+                        <span className="projects-chronology__division projects-chronology__division_right-indent"/>
+                        <span className="projects-chronology__division projects-chronology__division_right-indent"/>
+                    </div>
+                    <div className="projects-chronology__item">
+                        <span className="projects-chronology__division projects-chronology__division_left-indent"/>
+                        <span className="projects-chronology__division projects-chronology__division_left-indent"/>
+                        <span className="projects-chronology__division projects-chronology__division_left-indent"/>
+                        <span className="projects-chronology__division projects-chronology__division_wide">2015</span>
+                        <span className="projects-chronology__division projects-chronology__division_right-indent"/>
+                        <span className="projects-chronology__division projects-chronology__division_right-indent"/>
+                        <span className="projects-chronology__division projects-chronology__division_right-indent"/>
+                    </div>
+                    <div className="projects-chronology__item">
+                        <span className="projects-chronology__division projects-chronology__division_left-indent"/>
+                        <span className="projects-chronology__division projects-chronology__division_left-indent"/>
+                        <span className="projects-chronology__division projects-chronology__division_left-indent"/>
+                        <span className="projects-chronology__division projects-chronology__division_wide">2016</span>
+                        <span className="projects-chronology__division projects-chronology__division_right-indent"/>
+                        <span className="projects-chronology__division projects-chronology__division_right-indent"/>
+                        <span className="projects-chronology__division projects-chronology__division_right-indent"/>
+                    </div>
+                    <div className="projects-chronology__item">
+                        <span className="projects-chronology__division projects-chronology__division_left-indent"/>
+                        <span className="projects-chronology__division projects-chronology__division_left-indent"/>
+                        <span className="projects-chronology__division projects-chronology__division_left-indent"/>
+                        <span className="projects-chronology__division projects-chronology__division_wide">2017</span>
+                        <span className="projects-chronology__division projects-chronology__division_right-indent"/>
+                        <span className="projects-chronology__division projects-chronology__division_right-indent"/>
+                        <span className="projects-chronology__division projects-chronology__division_right-indent"/>
+                    </div>
+                    <div className="projects-chronology__item">
+                        <span className="projects-chronology__division projects-chronology__division_left-indent is-filled"/>
+                        <span className="projects-chronology__division projects-chronology__division_left-indent"/>
+                        <span className="projects-chronology__division projects-chronology__division_left-indent"/>
+                        <span className="projects-chronology__division projects-chronology__division_wide">2018</span>
+                        <span className="projects-chronology__division projects-chronology__division_right-indent"/>
+                        <span className="projects-chronology__division projects-chronology__division_right-indent"/>
+                        <span className="projects-chronology__division projects-chronology__division_right-indent"/>
+                    </div>
+                    <div className="projects-chronology__item">
+                        <span className="projects-chronology__division projects-chronology__division_left-indent is-filled"/>
+                        <span className="projects-chronology__division projects-chronology__division_left-indent is-filled"/>
+                        <span className="projects-chronology__division projects-chronology__division_left-indent is-filled"/>
+                        <span className="projects-chronology__division projects-chronology__division_wide is-filled">2019</span>
+                        <span className="projects-chronology__division projects-chronology__division_right-indent is-filled"/>
+                        <span className="projects-chronology__division projects-chronology__division_right-indent"/>
+                        <span className="projects-chronology__division projects-chronology__division_right-indent is-filled"/>
+                    </div>
                 </div>
             </section>
         );
