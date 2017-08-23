dbf8817f5c69a1bb21dca72925528466f780d746
Dmitry <makarenko.dmitry@wearesho.com>
2017-08-23 16:23:25 +0300

    Refactor after review

---
+++ config/config.local.js
@@ -0,0 +1,4 @@
+export const config = {
+    mail: "mail",
+    tel: "tel"
+};
---
+++ config/config.test.js
@@ -0,0 +1,6 @@
+const config = {
+    mail: "office@wearesho.com",
+    tel: "380 66 024-94-02",
+};
+
+module.exports = config;
