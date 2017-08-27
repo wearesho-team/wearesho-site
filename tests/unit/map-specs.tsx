fc6608103a5674a6973429f735c187adf58a66a0
Dmitry <makarenko.dmitry@wearesho.com>
2017-08-27 16:08:23 +0300

    Add test

---
+++ tests/unit/map-specs.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import {expect} from "chai";
+import {ReactWrapper, mount} from "enzyme";
+
+import {MapProps, Map} from "../../src/components/Widgets/Map";
+import GoogleMapReact from "google-map-react";
+import {Config} from "../../src/Config";
+
+describe("<Map/>", () => {
+
+    let wrapper: ReactWrapper<MapProps, any>;
+
+    beforeEach(() => wrapper = mount(<Map/>));
+
+    afterEach(() => wrapper.unmount());
+
+    // TODO: lang assertion
+
+    it("should contain GoogleMapReact with current lang and key props", () => {
+        expect((wrapper.find(GoogleMapReact).props().bootstrapURLKeys as any).key).to.equal(Config.mapApiKey);
+        expect(wrapper.find(GoogleMapReact).props().bootstrapURLKeys.language).to.equal("en");
+    });
+
+});
