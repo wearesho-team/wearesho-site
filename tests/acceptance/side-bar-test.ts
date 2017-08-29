561df7741656300bf55287a1c08b8960c96f187e
Dmitry <makarenko.dmitry@wearesho.com>
2017-08-29 13:48:43 +0300

    Fix side bar test

--- tests/acceptance/side-bar-test.ts
+++ tests/acceptance/side-bar-test.ts
@@ -7,11 +7,13 @@ Scenario("Menu navigation", async (I) => {
     I.amOnPage("/");
     I.waitForElement(".section-main", 30);
 
-    I.waitForElement(`.main-nav__link[href="/contact"]`);
+    I.waitForElement(`.main-nav__link[href="/contact"]`, 30);
+    I.wait(1);
     I.click(`.main-nav__link[href="/contact"]`);
     I.waitForElement(".section-partnership", 2);
 
-    I.waitForElement(`.main-nav__link[href="/"]`);
+    I.waitForElement(`.main-nav__link[href="/"]`, 30);
+    I.wait(1);
     I.click(`.main-nav__link[href="/"]`);
     I.waitForElement(".section-main", 2);
 });
