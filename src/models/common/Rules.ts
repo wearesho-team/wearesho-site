4ad1594ad855fe69b20021a65c758ae4d881b968
Dmitry <makarenko.dmitry@wearesho.com>
2017-10-11 17:55:48 +0300

    Fix KRESTIK

--- src/models/common/Rules.ts
+++ src/models/common/Rules.ts
@@ -3,15 +3,15 @@ export const PhoneRange = {
     min: 9
 };
 
+export const NameRange = {
+    max: 24,
+    min: 2
+};
+
 export const phonePattern = new RegExp(`^\\d{${PhoneRange.min},${PhoneRange.max}}$`);
-export const namePattern = /^[\sa-zA-ZА-Яа-яЄЇІєїіыЫёЁъЪ`'\\-]{2,}$/;
+export const namePattern = new RegExp(`^[\sa-zA-ZА-Яа-яЄЇІєїіыЫёЁъЪ'\\-]{${NameRange.min},${NameRange.max}}$`);
 
 export const TimeDefaults = {
     from: "09:00",
     to: "18:00"
-};
-
-export const NameRange = {
-    max: 24,
-    min: 2
 };
