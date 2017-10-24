389c08697bc8bbf8b1e272e4fd902d4f608abcfb
eugene_wearesho <grishchenko.eugene@wearesho.com>
2017-10-24 10:46:09 +0300

    Process page add to route

--- src/data/routeProps.ts
+++ src/data/routeProps.ts
@@ -1,11 +1,17 @@
 import {MainPage} from "../components/Pages/MainPage";
 import {PartnershipPage} from "../components/Pages/PartnershipPage";
+import {ProcessPage} from "../components/Pages/ProcessPage";
 
 export const routeProps = [
     {
         exact: true,
         path: "/",
         component: MainPage,
+    },
+    {
+        exact: false,
+        path: "/process",
+        component: ProcessPage,
     },
     {
         exact: false,
