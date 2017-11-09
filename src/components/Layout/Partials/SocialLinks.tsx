24638b55c6c222af092750eaadde7c3a0824755c
eugene_wearesho <grishchenko.eugene@wearesho.com>
2017-11-09 16:12:07 +0200

    Social links styles is corrected

--- src/components/Layout/Partials/SocialLinks.tsx
+++ src/components/Layout/Partials/SocialLinks.tsx
@@ -5,17 +5,36 @@ import {Config} from "../../../data/Config";
 import {GitIcon} from "../../Icons/GitIcon";
 import {BehanceIcon} from "../../Icons/BehanceIcon";
 import {LinkedinIcon} from "../../Icons/LinkedinIcon";
+import {TouchHover} from "../../../helpers/TouchHover";
 
 export const SocialLinks: React.SFC<undefined> = (): JSX.Element => {
     return (
         <div className="social-list">
-            <a href={Config.links.github} className="social-list__item" target="_blank" rel="nofollow noopener">
+            <a
+                href={Config.links.github}
+                className="social-list__item"
+                target="_blank"
+                rel="nofollow noopener"
+                {...TouchHover}
+            >
                 <GitIcon/>
             </a>
-            <a href={Config.links.behance} className="social-list__item" target="_blank" rel="nofollow noopener">
+            <a
+                href={Config.links.behance}
+                className="social-list__item"
+                target="_blank"
+                rel="nofollow noopener"
+                {...TouchHover}
+            >
                 <BehanceIcon/>
             </a>
-            <a href={Config.links.linkedin} className="social-list__item" target="_blank" rel="nofollow noopener">
+            <a
+                href={Config.links.linkedin}
+                className="social-list__item"
+                target="_blank"
+                rel="nofollow noopener"
+                {...TouchHover}
+            >
                 <LinkedinIcon/>
             </a>
         </div>
