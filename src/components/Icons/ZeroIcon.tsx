17b62fa42219742da9a464645e197846812f6717
eugene_wearesho <grishchenko.eugene@wearesho.com>
2017-11-10 15:07:56 +0200

    404 page number animation is added

--- src/components/Icons/ZeroIcon.tsx
+++ src/components/Icons/ZeroIcon.tsx
@@ -12,13 +12,6 @@ export const ZeroIcon: React.SFC<undefined> = () => (
         <filter id="shadow">
             <feDropShadow dx="0" dy="0" stdDeviation="0"/>
         </filter>
-        <path id="flashing" filter="url(#shadow)" fill="#EB4B7C" d="M77.5 0C34.8 0 0 34.8 0 77.5c0 33.8 21.8 62.6 52 73.2v-3.2c-28.6-10.4-49-37.9-49-70C3 36.4 36.4 3 77.5 3S152 36.4 152 77.5c0 32.1-20.4 59.6-49 70v3.2c30.2-10.6 52-39.4 52-73.2C155 34.8 120.2 0 77.5 0z"/>
-        {/*<animate*/}
-        {/*xlink:href="#flashing"*/}
-        {/*attributeName="fill"*/}
-        {/*from="1"*/}
-        {/*to=".5"*/}
-        {/*dur="1s"*/}
-        {/*/>*/}
+        <path filter="url(#shadow)" fill="#EB4B7C" d="M77.5 0C34.8 0 0 34.8 0 77.5c0 33.8 21.8 62.6 52 73.2v-3.2c-28.6-10.4-49-37.9-49-70C3 36.4 36.4 3 77.5 3S152 36.4 152 77.5c0 32.1-20.4 59.6-49 70v3.2c30.2-10.6 52-39.4 52-73.2C155 34.8 120.2 0 77.5 0z"/>
     </svg>
 );
