8a8ddcb8cfff862a6341e30e698eff37f7415ef2
Dmitry <human.getEmail@gmail.com>
2017-11-13 13:51:00 +0200

    Update active-point-specs.tsx

--- tests/unit/active-point-specs.tsx
+++ tests/unit/active-point-specs.tsx
@@ -23,7 +23,7 @@ describe("<ActivePoint/>", () => {
         onProjectChange
     };
 
-    beforeEach(async () => {
+    beforeEach(() => {
         wrapper = mount(<ActivePoint {...props}/>);
 
         node = wrapper.instance() as any;
