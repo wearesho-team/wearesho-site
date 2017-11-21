1fc5b370b405a99c5e5ea5af4b2b95d72554a4d7
eugene_wearesho <grishchenko.eugene@wearesho.com>
2017-11-21 16:32:15 +0200

    Tablet adaptability is finished

--- src/components/Pages/ProcessPage/ProcessPage.tsx
+++ src/components/Pages/ProcessPage/ProcessPage.tsx
@@ -14,43 +14,52 @@ export class ProcessPage extends BasePage {
                 <div className="align-container">
                     <h2 className="section__title">Процесс</h2>
                     <div className="section__half half_first">
-                        <h4 className="section__subtitle">Всего 5 этапов для полного цикла IT-услуг</h4>
-                        <p className="section__text">
-                            Или укажите свои контактные данные в форме ниже. Наши специалисты ответят на форме.
-                            Наши ответят на все ваши идиотские вопросы. Наши ответят на все ваши крайне.
-                        </p>
-                        <h5>Полную информацию вы можете увидеть в презентации</h5>
-                        <SubmitButton type="button" label="Скачать pdf"/>
+                        <div className="align-container">
+                            <h4 className="section__subtitle">
+                                Всего 5 этапов для полного&nbsp;
+                                <span>цикла IT-услуг</span>
+                            </h4>
+                            <p className="section__text">
+                                Или укажите свои контактные данные в форме ниже. Наши специалисты ответят на форме.
+                                Наши ответят на все ваши идиотские вопросы. Наши ответят на все ваши крайне.
+                            </p>
+                        </div>
+                        <div className="align-container">
+                            <h5>Полную информацию вы можете увидеть в презентации</h5>
+                            <SubmitButton type="button" label="Скачать pdf"/>
+                        </div>
                     </div>
                     <div className="section__half half_second">
-                        <div className="stage">
-                            <span className="stage__number marker glow">
-                                01
-                            </span>
-                            <div className="stage-body">
-                                <h3 className="stage__title">Проектирование</h3>
-                                <p className="stage__description">на основе данных</p>
-                                <span className="stage__detail">&gt;&gt;</span>
+                        <div className="align-container">
+                            <div className="stage">
+                                <span className="stage__number marker">
+                                    01
+                                </span>
+                                <div className="stage-body">
+                                    <h3 className="stage__title">Проектирование</h3>
+                                    <p className="stage__description">на основе данных</p>
+                                    <span className="stage__detail">&gt;&gt;</span>
+                                </div>
                             </div>
-                        </div>
-                        <div className="stage">
-                            <span className="stage__number marker">
-                                02
-                            </span>
-                            <div className="stage-body">
-                                <h3 className="stage__title">Дизайн</h3>
-                                <p className="stage__description">скетчи / прототипы</p>
-                                <span className="stage__detail">&gt;&gt;</span>
+                            <div className="stage">
+                                <span className="stage__number marker">
+                                    02
+                                </span>
+                                <div className="stage-body">
+                                    <h3 className="stage__title">Дизайн</h3>
+                                    <p className="stage__description">скетчи / прототипы</p>
+                                    <span className="stage__detail">&gt;&gt;</span>
+                                </div>
                             </div>
-                        </div>
-                        <div className="stage">
-                            <span className="stage__number marker">
-                                03
-                            </span>
-                            <div className="stage-body">
-                                <h3 className="stage__title">Разработка</h3>
-                                <p className="stage__description">front &amp; back end</p>
-                                <span className="stage__detail">&gt;&gt;</span>
+                            <div className="stage">
+                                <span className="stage__number marker">
+                                    03
+                                </span>
+                                <div className="stage-body">
+                                    <h3 className="stage__title">Разработка</h3>
+                                    <p className="stage__description">front &amp; back end</p>
+                                    <span className="stage__detail">&gt;&gt;</span>
+                                </div>
                             </div>
                         </div>
                         <div className="stages-group">
