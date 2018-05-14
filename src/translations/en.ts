e62bc460e6e80d6b7a69692222cbdbfc8059d89a
grishchenko-ev <grishchenko.ev86@gmail.com>
2018-05-14 13:45:10 +0300

    Bobra page clients-list alignment is corrected, translations is changed https://sho.myjetbrains.com/youtrack/issue/SHO-71

--- src/translations/en.ts
+++ src/translations/en.ts
@@ -8,7 +8,7 @@ translate.registerTranslations("en", {
     mainPage: {
         title: {
             mainOne: {
-                base: "Comprehensive",
+                base: "Complex",
                 additions: {
                     1: "IT-solutions",
                 }
@@ -25,9 +25,9 @@ translate.registerTranslations("en", {
         articles: {
             whatWeDo: {
                 upper: {
-                    title: "Implement:",
+                    title: "We implement:",
                     items: {
-                        1: "«Turn-key» online loan service",
+                        1: "«On a turnkey basis» on-line crediting service",
                     }
                 },
                 lower: {
