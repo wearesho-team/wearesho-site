920cf34d34a24f90e5d3a89a56f73f553c71a622
grishchenko-ev <grishchenko.ev86@gmail.com>
2018-06-08 18:59:07 +0300

    Services internal page layout almost finished

--- src/components/Pages/ServicesPage/ServicesPage.tsx
+++ src/components/Pages/ServicesPage/ServicesPage.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Develop } from "./Services";
+import { translate } from "../../../helpers/translate";
 
 export class ServicesPage extends React.Component {
 
@@ -10,7 +11,34 @@ export class ServicesPage extends React.Component {
                     <div className="section-main">
                         <Develop />
                     </div>
-                    <div className="section-aside" />
+                    <div className="section-aside">
+                        <ul className="aside-nav">
+                            <li>
+                                <span className="marker">1.</span>
+                                <h5>{translate("processPage.stages.title.planning")}</h5>
+                            </li>
+                            <li>
+                                <span className="marker">2.</span>
+                                <h5>{translate("processPage.stages.title.design")}</h5>
+                            </li>
+                            <li>
+                                <span className="marker">3.</span>
+                                <h5>{translate("processPage.stages.title.develop")}</h5>
+                            </li>
+                            <li>
+                                <span className="marker">4.</span>
+                                <h5>{translate("processPage.stages.title.deploy")}</h5>
+                            </li>
+                            <li>
+                                <span className="marker">5.</span>
+                                <h5>{translate("processPage.stages.title.promotion")}</h5>
+                            </li>
+                            <li>
+                                <span className="marker">6.</span>
+                                <h5>{translate("processPage.stages.title.support")}</h5>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </section>
         );
