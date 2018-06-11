e13cde3461e0fa7d04a9ff3e1348bc0ed1f4144c
grishchenko-ev <grishchenko.ev86@gmail.com>
2018-06-11 12:52:16 +0300

    Service inner page layout is added

--- src/components/Pages/ServicesPage/ServicesPage.tsx
+++ src/components/Pages/ServicesPage/ServicesPage.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import { Develop } from "./Services";
 import { translate } from "../../../helpers/translate";
 
+import { Link } from "react-router-dom";
+
 export class ServicesPage extends React.Component {
 
     public render() {
@@ -12,32 +14,44 @@ export class ServicesPage extends React.Component {
                         <Develop />
                     </div>
                     <div className="section-aside">
-                        <ul className="aside-nav">
-                            <li>
-                                <span className="marker">1.</span>
-                                <h5>{translate("processPage.stages.title.planning")}</h5>
-                            </li>
-                            <li>
-                                <span className="marker">2.</span>
-                                <h5>{translate("processPage.stages.title.design")}</h5>
-                            </li>
-                            <li>
-                                <span className="marker">3.</span>
-                                <h5>{translate("processPage.stages.title.develop")}</h5>
-                            </li>
-                            <li>
-                                <span className="marker">4.</span>
-                                <h5>{translate("processPage.stages.title.deploy")}</h5>
-                            </li>
-                            <li>
-                                <span className="marker">5.</span>
-                                <h5>{translate("processPage.stages.title.promotion")}</h5>
-                            </li>
-                            <li>
-                                <span className="marker">6.</span>
-                                <h5>{translate("processPage.stages.title.support")}</h5>
-                            </li>
-                        </ul>
+                        <div className="aside-nav">
+                            <Link to="/planning" className="stage">
+                                <span className="stage__number marker">01</span>
+                                <div className="stage-body">
+                                    <h3 className="stage__title">{translate("processPage.stages.title.planning")}</h3>
+                                </div>
+                            </Link>
+                            <Link to="/design" className="stage">
+                                <span className="stage__number marker">02</span>
+                                <div className="stage-body">
+                                    <h3 className="stage__title">{translate("processPage.stages.title.design")}</h3>
+                                </div>
+                            </Link>
+                            <Link to="/develop" className="stage active-control">
+                                <span className="stage__number marker">03</span>
+                                <div className="stage-body">
+                                    <h3 className="stage__title">{translate("processPage.stages.title.develop")}</h3>
+                                </div>
+                            </Link>
+                            <Link to="/deploy" className="stage">
+                                <span className="stage__number marker">04</span>
+                                <div className="stage-body">
+                                    <h3 className="stage__title">{translate("processPage.stages.title.deploy")}</h3>
+                                </div>
+                            </Link>
+                            <Link to="/promotion" className="stage">
+                                <span className="stage__number marker">05</span>
+                                <div className="stage-body">
+                                    <h3 className="stage__title">{translate("processPage.stages.title.promotion")}</h3>
+                                </div>
+                            </Link>
+                            <Link to="/support" className="stage">
+                                <span className="stage__number marker">06</span>
+                                <div className="stage-body">
+                                    <h3 className="stage__title">{translate("processPage.stages.title.support")}</h3>
+                                </div>
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </section>
